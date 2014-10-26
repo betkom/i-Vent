88d3254671b1b6789aa119b6d6f1060f762f5391
Chibuzor Obiora <odaiwrites@gmail.com>
2014-10-26 01:41:26 +0100

    consolidated mine and tolu's copies

--- app/controllers/likes.server.controller.js
+++ app/controllers/likes.server.controller.js
@@ -4,10 +4,41 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-	errorHandler = require('./errors'),
-	Like = mongoose.model('Like'),
-	_ = require('lodash');
+    errorHandler = require('./errors'),
+    Like = mongoose.model('Like'),
+    _ = require('lodash');
 
+/**
+ * Delete a Like
+ */
+exports.delete = function(req, res) {
+    var like = req.like;
+    var program = req.program;
+
+    var index = like && like._id ? program.likes.indexOf(like._id) : -1;
+    if (index > -1)
+        program.likes.splice(index, 1);
+
+    like.remove(function(err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            program.save(function(err) {
+                if (err)
+                    return res.status(400).send({
+                        message: errorHandler.getErrorMessage(err)
+                    });
+                else {
+                    like.like = false;
+                    res.jsonp(like);
+                }
+            });
+
+        }
+    });
+};
 
 function saveLike(req, res) {
         var like = new Like();
@@ -35,11 +66,9 @@ function saveLike(req, res) {
             }
         });
     }
-
-
-/**
- * Create a Like
- */
+    /**
+     * Create a Like
+     */
 exports.create = function(req, res) {
     Like.find({
         program: req.program,
@@ -51,7 +80,6 @@ exports.create = function(req, res) {
                 message: errorHandler.getErrorMessage(err)
             });
         } else {
-            console.log(likes, likes.length);
             if (likes.length === 0) {
                 saveLike(req, res);
                 //res.jsonp(likes);
@@ -67,64 +95,64 @@ exports.create = function(req, res) {
  * Show the current Like
  */
 exports.read = function(req, res) {
-	res.jsonp(req.like);
+    res.jsonp(req.like);
 };
-
 
 /**
- * Delete an Like
+ * Update a Like
  */
-exports.delete = function(req, res) {
-	var like = req.like;
-    var program = req.program;
+exports.update = function(req, res) {
+    var like = req.like;
 
-    var index = like && like._id ? program.likes.indexOf(like._id) : -1;
-    if (index > -1)
-        program.likes.splice(index, 1);
+    like = _.extend(like, req.body);
 
-
-	like.remove(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(like);
-		}
-	});
+    like.save(function(err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(like);
+        }
+    });
 };
+
 
 /**
  * List of Likes
  */
-exports.list = function(req, res) { Like.find({program:req.program}).sort('-created').populate('user', 'displayName').exec(function(err, likes) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(likes);
-		}
-	});
+exports.list = function(req, res) {
+    Like.find({
+        program: req.program
+    }).sort('-likes').populate('user', 'displayName').exec(function(err, likes) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(likes);
+        }
+    });
 };
 
 /**
  * Like middleware
  */
-exports.likeByID = function(req, res, next, id) { Like.findById(id).populate('user', 'displayName').exec(function(err, like) {
-		if (err) return next(err);
-		if (! like) return next(new Error('Failed to load Like ' + id));
-		req.like = like ;
-		next();
-	});
+exports.likeByID = function(req, res, next, id) {
+    Like.findById(id).populate('user', 'displayName').exec(function(err, like) {
+        if (err) return next(err);
+        if (!like) return next(new Error('Failed to load Like ' + id));
+        req.like = like;
+        next();
+    });
 };
 
 /**
  * Like authorization middleware
  */
 exports.hasAuthorization = function(req, res, next) {
-	if (req.like.user.id !== req.user.id) {
-		return res.status(403).send('User is not authorized');
-	}
-	next();
+    if (req.like.user.id !== req.user.id) {
+        return res.status(403).send('User is not authorized');
+    }
+    next();
 };
--- app/controllers/programs.server.controller.js
+++ app/controllers/programs.server.controller.js
@@ -4,7 +4,6 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-
     errorHandler = require('./errors'),
     Program = mongoose.model('Program'),
     Comment = mongoose.model('Comment'),
@@ -15,14 +14,12 @@ var mongoose = require('mongoose'),
  * Create a Program
  */
 exports.create = function(req, res) {
-
     // Sets default image
-    req.body.image = req.body.image && req.body.image[0] && req.body.image[0].length > 0 ? req.body.image : [{
+    req.body.image = req.body.image && req.body.image[0] && req.body.image[0].path.length > 0 ? req.body.image : [{
         path: '/modules/core/img/loaders/defaultimage.png'
     }];
     var program = new Program(req.body);
     program.user = req.user;
-
     program.save(function(err) {
         if (err) {
             return res.status(400).send({
@@ -38,7 +35,6 @@ exports.create = function(req, res) {
  * Show the current Program
  */
 exports.read = function(req, res) {
-
     if (req.user) {
         Like.find({
             user: req.user,
@@ -60,14 +56,13 @@ exports.read = function(req, res) {
 };
 
 
+
 /**
  * Update a Program
  */
 exports.update = function(req, res) {
     var program = req.program;
-
     program = _.extend(program, req.body);
-
     program.save(function(err) {
         if (err) {
             return res.status(400).send({
@@ -84,7 +79,6 @@ exports.update = function(req, res) {
  */
 exports.delete = function(req, res) {
     var program = req.program;
-
     program.remove(function(err) {
         if (err) {
             return res.status(400).send({
@@ -96,85 +90,50 @@ exports.delete = function(req, res) {
     });
 };
 
-
-var http = require('http');
-
-var testNexmo = function() {
-    var options = {
-        hostname: 'rest.nexmo.com',
-        port: 80,
-        path: '/call/json?api_key=5691ad12&api_secret=a8abd3c5&to=2348108006885&answer_url=https://africancampaigns.com/rest_api/call.vxml',
-        method: 'POST'
-    };
-
-    var req = http.request(options, function(res) {
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-        res.setEncoding('utf8');
-        res.on('data', function(chunk) {
-            console.log('BODY: ' + chunk);
-        });
-        req.on('error', function(e) {
-            console.log('problem with request: ' + e.message);
-        });
-
-    });
-
-    // write data to request body
-    req.write('data\n');
-    req.write('data\n');
-    req.end();
-};
-
-var testNexmoText = function() {
-    var options = {
-        hostname: 'rest.nexmo.com',
-        port: 80,
-        path: '/sms/json?api_key=5691ad12&api_secret=a8abd3c5&&from=iVent&to=2348108006885&text=D%C3%A9j%C3%A0%2Bvu',
-        method: 'POST'
-    };
-
-    var req = http.request(options, function(res) {
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-        res.setEncoding('utf8');
-        res.on('data', function(chunk) {
-            console.log('BODY: ' + chunk);
-        });
-        req.on('error', function(e) {
-            console.log('problem with request: ' + e.message);
-        });
-
-    });
-
-    // write data to request body
-    req.write('data\n');
-    req.write('data\n');
-    req.end();
-};
-
-
-var schedule = require('node-schedule');
-var testSchedule = function() {
-    var date = new Date(2014, 9, 25, 19, 40, 0);
-    console.log('in side test schedule');
-    var j = schedule.scheduleJob(date, function() {
-        console.log('The world is going to end today.', date);
-    });
-};
-
 /**
  * List of Programs
  */
-
 exports.list = function(req, res) {
-    Program.find().sort('-created').populate('user', 'displayName').exec(function(err, programs) {
+    Program.find().sort('-likes').populate('user', 'displayName').exec(function(err, programs) {
         if (err) {
             return res.status(400).send({
                 message: errorHandler.getErrorMessage(err)
             });
         } else {
             res.jsonp(programs);
+        }
+    });
+};
+exports.search = function(req, res) {
+    var $or = {
+        $or: []
+    };
+    var checkQuery = function() {
+        if (req.query.location && req.query.location.length > 0) {
+            $or.$or.push({
+                location: new RegExp(req.query.location, 'i')
+            });
+        }
+        if (req.query.category && req.query.category.length > 1) {
+            $or.$or.push({
+                category: new RegExp(req.query.category, 'i')
+            });
+        }
+        if (req.query.programDate && req.query.programDate.length > 1) {
+            $or.$or.push({
+                programDate: new RegExp(req.query.programDate)
+            });
+        }
+    };
+    checkQuery();
+    Program.find($or).exec(function(err, programs) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(programs);
+
         }
     });
 };

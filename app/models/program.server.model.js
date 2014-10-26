33ffa7be2f86eabf838da97600a01b7e889d16fe
Chibuzor Obiora <odaiwrites@gmail.com>
2014-10-26 04:31:58 +0000

    resolved issue with events near you functionality not working

--- app/models/program.server.model.js
+++ app/models/program.server.model.js
@@ -4,81 +4,74 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
+    Schema = mongoose.Schema;
 
 /**
  * Program Schema
  */
 var ProgramSchema = new Schema({
-	name: {
-		type: String,
-		default: '',
-		required: 'Please fill Event Title',
-		trim: true
-	},
-	programDate: {
-		type: String,
-		default: '',
-		required: 'Please fill date in this format yyyy/mm/dd',
-		trim: true
-	},
+    name: {
+        type: String,
+        default: '',
+        required: 'Please fill Event Title',
+        trim: true
+    },
+    programDate: {
+        type: String,
+        default: '',
+        required: 'Please fill date in this format yyyy/mm/dd',
+        trim: true
+    },
 
-	programTimeMinute: {
-	type: String,
-	default: '',
-	required: 'Please fill minutes mm',
-	},
+    programTime: {
+        type: String,
+        default: '',
+        required: 'Please fill minutes mm',
+    },
 
-	programTimeHour: {
-	type: String,
-	default: '',
-	required: 'Please fill hour hh',
-	trim: true
-	},
+    category: {
+        type: String,
+        default: '',
+        required: 'Please choose a category',
+    },
+    description: {
+        type: String,
+        default: '',
+        required: 'Please fill a description',
+        trim: true
+    },
 
-	category: {
-		type: String,
-		default: '',
-		required: 'Please choose a category',
-	},
-	description: {
-		type: String,
-		default: '',
-		required: 'Please fill a description',
-		trim: true
-	},
+    location: {
+        type: String,
+        default: '',
+        required: 'Please fill the location',
+        trim: true
+    },
 
-	location: {
-		type: String,
-		default: '',
-		required: 'Please fill the location',
-		trim: true
-	},
+    image: [{
+        path: {
+            type: String,
+            default: ''
+        }
+    }],
 
-	image: [{
-		path:{
-			type: String,
-			default: ''
-		}
-	}],
+    likes: [{
+        type: Schema.ObjectId,
+        ref: 'Like'
+    }],
 
-	likes: [{
-		type: Schema.ObjectId,
-		ref: 'Like'
-	}],
-
-	comments: [{
-		type: Schema.ObjectId,
-		ref: 'Comment'
-	}],
-	created: {
-		type: Date,
-		default: Date.now
-	},
-	user: {
-		type: Schema.ObjectId,
-		ref: 'User'
-	}
+    comments: [{
+        type: Schema.ObjectId,
+        ref: 'Comment'
+    }],
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    user: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    }
 });
 
 mongoose.model('Program', ProgramSchema);
